d6b10c9d7e71d20db196b75e3dcfa2c76a254f06
huangenai <huangenailq@outlook.com>
2019-01-08 14:50:54 +0800

    no message

--- www/apsNotification.js
+++ www/apsNotification.js
@@ -1,6 +1,14 @@
 
-var exec = require('cordova/exec'),fzPush = function() {};
-fzPush.prototype.getCordovaIntent = function(successCallback, failureCallback) {
+var exec = require('cordova/exec'),ApsNotification = function() {};
+
+/**
+    成功和失败的回调，各有一个字符串参数，分别是deviceToken和错误信息
+**/
+ApsNotification.prototype.registerRemoteApsNotification = function(success, fail) {
+    return exec(success, fail, "ApsNotificationPlugin", "registerRemoteApsNotification", []);
+};
+
+ApsNotification.prototype.getCordovaIntent = function(successCallback, failureCallback) {
     return exec (
         successCallback,
         failureCallback,
@@ -10,8 +18,4 @@ fzPush.prototype.getCordovaIntent = function(successCallback, failureCallback) {
     );
 };
 
-fzPush.prototype.registerRemoteApsNotification = function(success, fail) {
-    return exec(success, fail, "ApsNotificationPlugin", "registerRemoteApsNotification", []);
-};
-
-module.exports = new fzPush();
+module.exports = new ApsNotification();
